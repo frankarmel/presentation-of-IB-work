1d8b0e6765f56a047fa78e852820f1a6bd08838d
frankarmel <fyoudom100@gmail.com>
2024-09-11 17:55:28 +0100

    link off project to my github

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    @Test
+    void setup() {
+
+        WebDriver webDriver = new EdgeDriver();
+
+
+    }
+}
