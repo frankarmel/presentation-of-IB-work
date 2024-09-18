76587af78f23f60f4358917ebdf090c5b969663f
frankarmel <fyoudom100@gmail.com>
2024-09-18 19:21:48 +0100

    js modification

--- src/test/java/DefaultSuiteTest.java
+++ src/test/java/DefaultSuiteTest.java
@@ -3,21 +3,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.WheelInput;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.*;
-
-
 public class DefaultSuiteTest {
     private WebDriver driver;
-    JavascriptExecutor js;
+
     @BeforeTest
     public void setUp() {
         driver = new ChromeDriver();
-        js = (JavascriptExecutor) driver;
     }
 
     @AfterTest
@@ -29,34 +25,35 @@ public class DefaultSuiteTest {
     public void test1() {
         // Test name: test1
         // Step # | name | target | value
-        // 1 | open | /fr/translator |
+        // 1 | open |
         driver.get("https://www.deepl.com/fr/translator");
-        // 2 | runScript | window.scrollTo(0,0) |
-       js.executeScript("window.scrollTo(0,0)");
-        // 3 | click | css=.ProductNavigation-module--container--34e1a |
+        // 2 | scroll to top |
+        WheelInput.ScrollOrigin origin = WheelInput.ScrollOrigin.fromViewport(0, 0);
+        new Actions(driver).scrollFromOrigin(origin, 0, 0);
+        // 3 | click |
         driver.findElement(By.cssSelector(".ProductNavigation-module--container--34e1a")).click();
-        // 4 | click | id=document-tab-heading |
+        // 4 | click |
         driver.findElement(By.id("document-tab-heading")).click();
-        // 5 | click | id=text-tab-heading |
+        // 5 | click |
         driver.findElement(By.id("text-tab-heading")).click();
-        // 6 | mouseOver | id=text-tab-heading |
+        // 6 | mouseOver |
         {
             WebElement element = driver.findElement(By.id("text-tab-heading"));
             Actions builder = new Actions(driver);
             builder.moveToElement(element).perform();
         }
-        // 7 | mouseOut | id=text-tab-heading |
+        // 7 | mouseOut |
         {
             WebElement element = driver.findElement(By.tagName("body"));
             Actions builder = new Actions(driver);
             builder.moveToElement(element, 0, 0).perform();
         }
-        // 8 | click | css=.min-h-0 > div:nth-child(1) |
+        // 8 | click |
         driver.findElement(By.cssSelector(".min-h-0 > div:nth-child(1)")).click();
-        // 9 | editContent | css=.min-h-0 > div:nth-child(1) | <p _d-id="4">test</p>
+        // 9 | editContent |
         {
             WebElement element = driver.findElement(By.cssSelector(".min-h-0 > div:nth-child(1)"));
-            js.executeScript("if(arguments[0].contentEditable === 'true') {arguments[0].innerText = '<p _d-id=\"4\">test</p>'}", element);
+            //js.executeScript("if(arguments[0].contentEditable === 'true') {arguments[0].innerText = '<p _d-id=\"4\">test</p>'}", element);
         }
     }
 }
